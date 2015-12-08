bbeaeba9e70786d7f4c5c7630796b10d77833fc7
joe2016 <jo_huang@foxmail.com>
2015-12-08 15:57:27 +0800

    1st version

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication2.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"this is written by joe, 2015.12.08"<<endl;
+	return 0;
+}
+
